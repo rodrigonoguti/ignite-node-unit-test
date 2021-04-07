933ed3d0ab410ec79a9695643adfb1689d16334f
Rodrigo Haruo Noguti <rodrigonoguti@gmail.com>
2021-04-07 16:49:26 -0300

    challenge done

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,7 @@
 export default {
   jwt: {
-    secret: process.env.JWT_SECRET as string,
+    // secret: process.env.JWT_SECRET as string,
+    secret: "senhasupersecreta123",
     expiresIn: '1d'
   }
 }
