fe8bfc857cfccd74e813eb7c2aec7a7fc2258ca3
Rodrigo Haruo Noguti <rodrigonoguti@gmail.com>
2021-04-10 21:30:41 -0300

    challenge done

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,7 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection } from 'typeorm';
 
-(async () => await createConnection())();
+// (async () => await createConnection())();
+
+export default async (): Promise<Connection> => {
+  return createConnection();
+};
